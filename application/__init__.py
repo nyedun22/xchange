ec4175f6da0dc2144f38650de1abcb02de118aef
Natasha Edun <natashaedun@Natashas-MacBook-Pro-2.local>
2022-08-06 19:11:00 +0100

    starter code for init file

---
+++ application/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# create a new instance of Flask and store it in app
+app = Flask(__name__)
+
+#app.config code to link to db server goes here as per the copy___init__ file example:
