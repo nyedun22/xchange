738288244eeaf574d10e6debb783234c18f9c52f
Juliette <98818137+JulietteBehr@users.noreply.github.com>
2022-08-09 18:43:06 +0100

    Updated App & Forms for flash

--- application/app.py
+++ application/app.py
@@ -1,13 +1,14 @@
 #from application import app
 # import the ./application/routes.py file
 #from application import routes
-from db_models import user_details
+from db_models import user_details, bank_details
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 # from flask_migrate import Migrate
 from forms import CustomerRegistrationForm, LoginForm, CurrencyForm
 from sqlalchemy.orm import Session
+import random
 # from werkzeug.security import check_password_hash
 # from flask_login import (
 #     UserMixin,
@@ -46,18 +47,11 @@ def create_app():
 
 app = create_app()
 
-
 # Home route
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home_not_logged_in.html')
-
-
-@app.route('/home_logged_in')
-def home_logged_in():
-    return render_template('home.html')
-
 
 # Login route
 @app.route('/login', methods=['GET', 'POST'])
@@ -71,14 +65,15 @@ def user_login():
             user = user_details.query.filter_by(username=input_username[0]).first()
             if str(user.pass_word) == str(input_password):
                 #login_user(user)
+                flash('Login successful.', category='success')
                 return redirect(url_for('currency_convertor'))
             else:
                 raise Exception
         except:
+            flash('Incorrect username or password, please try again.', category='error')
             return render_template('login.html', form=form)
 
     return render_template('login.html', form=form)
-
 
 # Register route
 @app.route('/register', methods=['GET', 'POST'])
@@ -94,12 +89,24 @@ def user_sign_up():
         address = request.form['address'],
         postcode = request.form['postcode']
 
+        flash(f'Account created for {form.username.data}!', category='success')
+
         #secure_password = bcrypt.generate_password_hash(password[0])
 
         new_user_details = user_details(first_name=first_name[0], last_name=last_name[0], email=email[0],
                                         address_line_1=address[0], postcode=postcode, username=username[0], pass_word=password[0])
         db.session.add(new_user_details)
         db.session.commit()
+
+        # Find way to set user_id from user_details into bank_details
+        # user = user_details.query.filter_by(username=username[0]).first()
+        # new_user_id = user_details.user_id
+        # new_account_number = random.randint(10000000, 99999999)
+        # new_sort_code = random.randint(100000, 999999)
+        # new_user_bank = bank_details(user_id=100, account_number=new_account_number, sort_code=new_sort_code, main_account_balance=1000)
+        # db.session.add(new_user_bank)
+        # db.session.commit()
+
         return redirect(url_for('user_login'))
 
     return render_template('register.html', form=form)
@@ -115,9 +122,6 @@ def user_sign_up():
         #     error = "Please complete each section of this form"
         #     return render_template('home.html', title='Home', form=form)
         #return render_template('register.html', title='Register', form=form)  # message=error
-    #
-    # if form.validate():
-    #     flash(f'Account created for {form.username.data}!', 'success')
 
     # else:
     #     return render_template('register.html', form=form)
@@ -133,16 +137,13 @@ def currency_convertor():
         return redirect(url_for('transactions', gbp_code=gbp, dropdown_code=dropdown))
     return render_template('currency.html', form=form)
 
-
 @app.route('/transactions')
 def transactions():
      gbp_code = request.args.get('gbp_code')
      dropdown_code = request.args.get('dropdown_code')
-    #the functions in api_file need to be adapted so that we can pass the gbp_code and
-     # dropwdown_code variables into this new route. Meaning that instead
+    #the functions in api_file need to be adapted so that we can pass the gbp_code and dropwdown_code variables into this new route. Meaning that instead
     # showing print statements, return statements are  used. This is a work in progress.
      return render_template('transactions.html')
-
 
 if __name__ == '__main__':
     # app.run(debug=True, host='0.0.0.0')
