afcf5e7b1b0e64b992144f47a97202cbdfa56926
giannenandra <giannesnandra@gmail.com>
2022-08-09 21:29:55 +0100

    moved initialising the flask app to here

--- application/__init__.py
+++ application/__init__.py
@@ -1,22 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import getenv
 
-# create a new instance of Flask and store it in app
-app = Flask(__name__)
+db = SQLAlchemy()
 
-#app.config code to link to db server goes here as per the copy___init__ file example:
-#  ORM Object Relational Mapper
-#  Object is Python classes
-#  Relational is MySQL
-# SQLAlchemy is the MAPPING layer between them
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost/xchange"
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://username:password@host/database_name"
-# app.config['SQLALCHEMY_DATABASE_URI']=('mysql+pymysql://' + getenv('MYSQL_USER') + ':' + getenv('MYSQL_PASSWORD') + '@' + getenv('MYSQL_HOST') + '/' + getenv('MYSQL_DB'))
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
-# app.config['SECRET_KEY'] = getenv('FLASK_SECRETKEY')
+def create_app():
+    app = Flask(__name__)
 
+    app.secret_key = 'secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# link our app to the persistence layer
-db = SQLAlchemy(app)
+    db.init_app(app)
+
+    return app
